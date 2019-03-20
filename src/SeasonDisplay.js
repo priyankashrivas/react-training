3386490bce829ccc23b35e794a8fa6f2e883d3ca
priyankashrivas <priyanka.shriwas@systematixindia.com>
2019-03-20 12:14:50 +0530

    initial commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,42 @@
+import './Seasondisplay.css';
+import React from 'react';
+
+
+const seasonConfig = {
+     summer:{
+          text : 'Lets hit the beach',
+          iconName : 'sun'
+     },
+     winter:{
+          text : 'Indore: Itni thand saala admi jhnd....!',
+          iconName : 'snowflake'
+     }
+};
+const getSeason = (lat,month) => {
+    
+     if(month>2 && month<9){
+          return lat>0 ? 'summer' : 'winter';
+     }
+     else{
+         return lat>0 ? 'winter' : 'summer';
+     }
+};
+
+const SeasonDisplay =props =>{
+     const season = getSeason(props.lat, new Date().getMonth());
+     console.log(props.lat);
+     console.log(season);
+     const{text,iconName}=seasonConfig[season]; //{text,iconName}
+     
+     return (
+          <div className={`season-display ${season}`}>
+               <i className ={`icon-left massive ${iconName} icon`} />
+               <h1>{text}</h1>
+               <i className ={`icon-right massive ${iconName} icon`} />
+          </div>
+
+     );
+};
+
+
+export default SeasonDisplay;
